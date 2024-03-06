623936bdab0730e36c7ec10a5f7848f0fae86768
MisterKyryl <99047616+MisterKyryl@users.noreply.github.com>
2024-03-06 13:53:08 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,95 @@
+"use strict"
+
+// Многоуровневое меню
+const isMobile = {
+   Android: function () {
+      return navigator.userAgent.match(/Android/i);
+   },
+   BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+   },
+   iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+   },
+   Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+   },
+   Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+   },
+   any: function () {
+      return (
+         isMobile.Android() ||
+         isMobile.BlackBerry() ||
+         isMobile.iOS() ||
+         isMobile.Opera() ||
+         isMobile.Windows());
+   }
+};
+
+if (isMobile.any()) {
+   document.body.classList.add('_touch');
+   // Анимация стрелок
+   let menuArrows = document.querySelectorAll('.menu__arrow');
+   if (menuArrows.length > 0) {
+      for (let index = 0; index < menuArrows.length; index++) {
+         const menuArrow = menuArrows[index];
+         menuArrow.addEventListener("click", function (e) {
+            menuArrow.parentElement.classList.toggle('_active');
+         });
+      }
+   }
+
+} else {
+   document.body.classList.add('_pc');
+}
+
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+   const menuBody = document.querySelector('.menu__body');
+   iconMenu.addEventListener("click", function (e) {
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+   })
+}
+
+// Slider
+const swiper = new Swiper('.slider-main-block', {
+	// Optional parameters
+	loop: true,
+	effect: 'fade',
+	fadeEffect: {
+		crossFade: true,
+	},
+	// Navigation arrows
+	navigation: {
+		nextEl: '.body-main-block__arrow.swiper-button-next',
+		prevEl: '.body-main-block__arrow.swiper-button-prev',
+	},
+});
+
+
+// Tabs
+const tabNavItems = document.querySelectorAll('.tabs-deals__button');
+const tabItems = document.querySelectorAll('.item-tabs');
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+	let currentActiveIndex = null;
+	let newActiveIndex = null;
+	if (targetElement.closest('.tabs-deals__button')) {
+		tabNavItems.forEach((tabNavItem, index) => {
+			if (tabNavItem.classList.contains('active')) {
+				currentActiveIndex = index;
+				tabNavItem.classList.remove('active');
+			}
+			if (tabNavItem === targetElement) {
+				newActiveIndex = index;
+			}
+		});
+		targetElement.classList.add('active');
+		tabItems[currentActiveIndex].classList.remove('active');
+		tabItems[newActiveIndex].classList.add('active');
+	}
+});
